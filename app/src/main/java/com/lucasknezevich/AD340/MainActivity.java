548afce4d2aced0f96fcc976f034276dc0a612b3
LucasKnezevich <knezevich.lucas@gmail.com>
2022-04-17 14:55:06 -0700

    GridView added with buttons populated by an array.

--- app/src/main/java/com/lucasknezevich/AD340/MainActivity.java
+++ app/src/main/java/com/lucasknezevich/AD340/MainActivity.java
@@ -2,12 +2,18 @@ package com.lucasknezevich.AD340;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] btn_names_main = {"Add Event", "Calendar", "Gear List", "Account Info"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +26,61 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast toast_login = Toast.makeText(getApplicationContext(),
-                        getString(R.string.button_submit),Toast.LENGTH_LONG);
+                        getString(R.string.button_submit),Toast.LENGTH_SHORT);
                 toast_login.show();
             }
         });
 
 
+        GridView main_grid = findViewById(R.id.main_gridview);
+        main_grid.setAdapter(new ButtonAdapter(this));
+
+
+    }
+
+    public class ButtonAdapter extends BaseAdapter {
+
+        private Context myContext;
+
+        public ButtonAdapter(Context context) {
+            myContext = context;
+        }
+
+        @Override
+        public int getCount() {
+            return btn_names_main.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int index, View convertView, ViewGroup parent) {
+            Button button;
+            if (convertView == null) {
+                button = new Button(myContext);
+            } else {
+                button = (Button) convertView;
+            }
+
+            button.setText(btn_names_main[index]);
+            button.setId(index);
+             button.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View view) {
+                     Toast toast = Toast.makeText(myContext,btn_names_main[index],Toast.LENGTH_SHORT);
+                     toast.show();
+                 }
+             });
+            return button;
+        }
     }
 
 
