fa076bd06569cf66d993781e84af2059c1feff16
LucasKnezevich <knezevich.lucas@gmail.com>
2022-04-17 14:05:11 -0700

    Added Toast for Login button

--- app/src/main/java/com/lucasknezevich/AD340/MainActivity.java
+++ app/src/main/java/com/lucasknezevich/AD340/MainActivity.java
@@ -3,6 +3,9 @@ package com.lucasknezevich.AD340;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginBtn = findViewById(R.id.button_login);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast_login = Toast.makeText(getApplicationContext(),
+                        getString(R.string.button_submit),Toast.LENGTH_LONG);
+                toast_login.show();
+            }
+        });
+
+
     }
 
 
